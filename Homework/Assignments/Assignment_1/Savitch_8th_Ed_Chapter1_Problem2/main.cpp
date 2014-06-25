82c0a71a1e89592e4dc534e164b257359c5e1b42
pookjo3 <jliso152@gmail.com>
2014-06-24 21:27:18 -0700

    Signed-off-by: pookjo3 <jliso152@gmail.com>

---
+++ Homework/Assignments/Assignment_1/Savitch_8th_Ed_Chapter1_Problem2/main.cpp
@@ -0,0 +1,51 @@
+/* 
+ * File:   main.cpp
+ * Author: Jonathon Lisonbee
+ * Created on June 24, 2014, 9:20 PM
+ * Purpose: Homework Assignment 1
+ */
+
+//System Library
+#include <iostream>
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins
+int main(int argc, char** argv) {
+//Define variables
+    int numbPod, peaPerPd, totalP;
+    
+    //Enter in Values for Peas and Pods
+    cout << "Hello.\n";
+    cout << "Press RETURN after entering a number.\n";
+    cout << "Enter number of Pods:\n";
+    
+    cin >> numbPod;
+    
+    cout << "Enter number of Peas in a Pod:\n";
+    
+    cin >> peaPerPd;
+    
+    //Calculate total peas
+    totalP = numbPod * peaPerPd;
+    
+    //Output
+    cout << "If you have ";
+    cout << numbPod;
+    cout << " pea pods\n";
+    cout << "and ";
+    cout << peaPerPd;
+    cout << " peas in each pod, then\n";
+    cout << "you have ";
+    cout << totalP;
+    cout << " peas in all the pods.\n";
+    cout << "Goodbye!\n";
+    
+    //le FIN
+    return 0;
+}
