fafb1fe1d5a4b11c67a621fbbfbebc9320548c90
pookjo3 <jliso152@gmail.com>
2014-06-24 19:09:49 -0700

    Signed-off-by: pookjo3 <jliso152@gmail.com>

---
+++ Homework/Hello_World/main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * Hello World
+ * File:   main.cpp
+ * Author: Jonathon Lisonbee
+ * Purpose: First Program
+ * Created on June 23, 2014, 12:22 PM
+ */
+//System Level Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Functon Prototypes
+
+//Execution Begins
+int main(int argc, char** argv) {
+    //Output Simple text
+    cout << "Hello World" << endl;
+    return 0;
+}
+
