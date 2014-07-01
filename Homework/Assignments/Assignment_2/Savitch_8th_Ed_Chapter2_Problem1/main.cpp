b5fbc5247bb6386a32aab715a958d6d9d9c0d620
pookjo3 <jliso152@gmail.com>
2014-07-01 11:22:00 -0700

    Signed-off-by: pookjo3 <jliso152@gmail.com>

---
+++ Homework/Assignments/Assignment_2/Savitch_8th_Ed_Chapter2_Problem1/main.cpp
@@ -0,0 +1,42 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on July 1, 2014, 10:30 AM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+const float TON_OUNCE = 35273.92f; //Metric ton in ounces
+
+//Function Prototypes
+
+//Execution Begins
+int main(int argc, char** argv) {
+    //Declare Variables
+    float cerWght, //Weight of the Cereal
+          wghtTon, //Weight of Cereal in tons
+          numbBox; //Number of boxes in one ton
+  
+       
+    //Input
+    cout<<"Please input the weight of the cereal in ounces:"<<endl;
+    cin>>cerWght;
+    
+    //Calculations
+    wghtTon = cerWght / TON_OUNCE;
+    numbBox = TON_OUNCE / cerWght;
+    
+    //Output
+    cout<<"The weight of the cereal in metric tons is "<<wghtTon<<endl;
+    cout<<"There are "<<numbBox<<" boxes of cereal in one metric ton."<<endl;
+    cout<<"Press Ctrl+C to exit at any time.\n";
+ 
+    return 0;
+}
+
