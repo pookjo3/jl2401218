bdf37405df3b8aff2082c480fbee57a5a6237713
pookjo3 <jliso152@gmail.com>
2014-07-02 22:02:30 -0700

    Signed-off-by: pookjo3 <jliso152@gmail.com>

--- Homework/Assignments/Assignment_2/Savitch_8th_Ed_Chapter2_Problem3/main.cpp
+++ Homework/Assignments/Assignment_2/Savitch_8th_Ed_Chapter2_Problem3/main.cpp
@@ -1,19 +1,40 @@
 /* 
  * File:   main.cpp
- * Author: JJ
- *
- * Created on July 2, 2014, 5:54 PM
+ * Author: Jonathon Lisonbee
+ * Created on July 2, 2014
+ * Savitch Chapter 2 Problem 10
  */
 
-#include <cstdlib>
-
+//System Libraries
+#include <iostream>
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+//Global Constants
+const float PAY_RISE = 7.6e-2; //Pay raise
 
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+     //Define Variable
+    float oldSal, newSal, payRet, monSal;
+    
+    //Inputs
+    cout<<"What is your old salary?\n";
+    cin>>oldSal;
+        
+    //Calculations
+    newSal = oldSal *(1+PAY_RISE);
+    
+    payRet = (newSal-oldSal)/(12/6);
+    
+    monSal = newSal / 12;
+    
+    //Output
+    cout<<"Retro Active Pay Due: $"<<payRet<<endl;
+    cout<<"New Annual Salary:    $"<<newSal<<endl;
+    cout<<"New Monthly Salary:   $"<<monSal<<endl;
+
+    //le Fin
     return 0;
 }
-
