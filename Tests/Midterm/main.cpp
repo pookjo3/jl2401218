bbc7335f61f36415e7116eb40b6ad0a3fe357fdf
pookjo3 <jliso152@gmail.com>
2014-07-16 09:34:18 -0700

    Signed-off-by: pookjo3 <jliso152@gmail.com>

--- Tests/Midterm/main.cpp
+++ Tests/Midterm/main.cpp
@@ -9,6 +9,7 @@
 #include <ctime>
 #include <iomanip>
 #include <cmath>
+#include <string>
 using namespace std;
 
 //Global Constants
@@ -64,7 +65,7 @@ int getN(){
 //Solution to problem 1
 void problem1(){
     //Define Variables
-    int input;
+    unsigned int input;
     
     //Ask for inputs
     cout<<"Please choose a positive number to display the countdown."<<endl;
@@ -105,7 +106,46 @@ void problem2(){
 
 //Solution to problem 3
 void problem3(){
-    cout<<"In problem # 3"<<endl<<endl;
+    //Declare Variables
+    char pin[6];
+    unsigned short inBal,   //Initial Balance 
+            debits,         //All debits for the month
+            credits;        //All credits for the month
+    float totBal;           //Total balance
+    
+    //Input info and validate pin
+    
+    cout<<"Please enter your 4 digit PIN:"<<endl;
+    cin>>pin;
+    
+    while (!(str.size(pin)==4)){
+        cout<<"Please input a 4 digit PIN:"<<endl;
+        cin>>pin;
+    }
+    
+    cout<<"What is your initial balance?"<<endl;
+    cin>>inBal;
+    
+    cout<<"What is the total about of debits charged for the month?"<<endl;
+    cin>>debits;
+    
+    cout<<"What is the total amount of credit added for the month?"<<endl;
+    cin>>credits;
+    
+    //Check new balance for overdraw, calculate total balance
+    if(debits>(credits+inBal)){     //If balance is negative
+        totBal=credits+inBal-debits-27.75f;
+        
+        cout<<"Your account has been overdrawn. A fee of "<<endl;
+              "$27.75 has been applied. In order to charge"<<endl;
+              "more debits to your account, please zero out"<<endl;
+              "the balance and add more credit. Thank you."<<endl<<endl;
+    }else{                          //If balance is positive
+        totBal=credits+inBal-debits;
+    }
+    
+    //Output new balance
+    cout<<"Your balance is $"<<totBal<<endl<<endl;
 }
 
 //Solution to problem 4
@@ -123,7 +163,7 @@ void problem5(){
     cout<<"How many hours did you work?"<<endl;
     cin>>hours;
     
-    cout<<"What is your pay rate?\n";
+    cout<<"What is your pay rate?"<<endl;
     cin>>payRate;
     
     //Determine total payment
